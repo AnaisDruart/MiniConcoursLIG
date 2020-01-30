96bf952111c3473b6ade673924615e12ee274c56
KiwiiXaos <43905857+KiwiiXaos@users.noreply.github.com>
2020-01-30 01:22:48 +0100

    Add files via upload

---
+++ Assets/Scripts/MechantHolder.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MechantHolder : MonoBehaviour
+{
+    public GameObject mechant;
+    
+    
+    public GameObject Enemy;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+
+  
+    public float spawnRate = 0.2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    
+
+    }
+    IEnumerator EnemyDrop()
+    {
+        while (enemyCount < 10)
+        {
+            xPos = Random.Range(12, 38);
+            zPos = Random.Range(15, 39);
+            Instantiate(Enemy, new Vector3(xPos, 20, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(4f);
+            enemyCount += 1;
+        }
+
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/tapisbouge.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tapisbouge : MonoBehaviour {
+	public GameObject TapisRoulant;
+	public Transform endpoint;
+	public float speed;
+	// Use this for initialization
+	void OnTriggerStay (Collider other) {
+		other.transform.position = Vector3.MoveTowards(other.transform.position, endpoint.position, speed * Time.deltaTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
