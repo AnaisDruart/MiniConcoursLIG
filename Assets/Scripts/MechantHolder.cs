87b64617b7a604b538cb07d7eb9efe86f7519825
KiwiiXaos <43905857+KiwiiXaos@users.noreply.github.com>
2020-01-30 18:44:39 +0100

    Add files via upload

--- Assets/Scripts/MechantHolder.cs
+++ Assets/Scripts/MechantHolder.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 
 public class MechantHolder : MonoBehaviour
 {
-    public GameObject mechant;
     
-    
-    public GameObject Enemy;
+    public GameObject Enemy1;
+    public GameObject Enemy2;
+    public GameObject Enemy3;
+    public GameObject Enemy4;
     public int xPos;
     public int zPos;
     public int enemyCount;
+    public int rdmenemy;
 
   
     public float spawnRate = 0.2f;
@@ -26,10 +28,38 @@ public class MechantHolder : MonoBehaviour
     }
     IEnumerator EnemyDrop()
     {
+        GameObject Enemy = Enemy1;
         while (enemyCount < 10)
         {
+            rdmenemy = Random.Range(1, 4);
+            if(rdmenemy == 1)
+            {
+                 Enemy = Enemy1;
+            }
+            else
+            {
+                if (rdmenemy == 2)
+                {
+                    Enemy = Enemy2;
+                }
+                else
+                {
+                    if (rdmenemy == 3)
+                    {
+                        Enemy = Enemy3;
+                    }
+                    else
+                    {
+                        if (rdmenemy == 4)
+                        {
+                             Enemy = Enemy4;
+                        }
+                    }
+                }
+            }
             xPos = Random.Range(12, 38);
             zPos = Random.Range(15, 39);
+
             Instantiate(Enemy, new Vector3(xPos, 20, zPos), Quaternion.identity);
             yield return new WaitForSeconds(4f);
             enemyCount += 1;
